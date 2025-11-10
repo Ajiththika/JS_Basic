5abe7e6763e6e0414dbe00e28733ab1fbc03f28d
Ajiththika <ajiththika17@gmail.com>
2025-11-10 11:43:57 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,20 @@
+{
+    var x=25;
+    console.log(x);
+}
+console.log(x);
+
+{
+    let x=25;
+    console.log(x);
+}
+console.log(x);
+
+{
+    const x=25;
+    console.log(x);
+}
+console.log(x);
+
+console.log(y);
+let y=20;
