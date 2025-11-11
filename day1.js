5624d2a66f150d5b332072d4e4e62b5fc5cf713a
Ajiththika <ajiththika17@gmail.com>
2025-11-11 08:24:43 +0530

    second commit

--- day1.js
+++ day1.js
@@ -1,3 +1,19 @@
+console.log('Welcome to JavaScript');
+
+//Declare and initialize a variable
+let x = 10;
+console.log(x);
+
+//Declare a variable
+let y;
+console.log(y);
+
+//Initialize the variable
+y = 20;
+console.log(y);
+
+//Hoisting
+
 {
     var x=25;
     console.log(x);
